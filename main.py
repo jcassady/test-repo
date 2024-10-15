fe48c5abe9624dcf7ff468716fd53647e7a7d943
Jordan Cassady <jordan.cassady@gmail.com>
2024-10-15 13:23:14 +0000

    Add sample code

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+def greet(name):
+    print(f'Hello, {name}')
+
+greet('Copilot')
+
+def add_numbers(a, b)
+    return a + b
+
+result = add_numbers(3, 4)
+print('Result:', result)

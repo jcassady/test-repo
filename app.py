278eb4fad0cf967f95d2d50d66e5285432d5156c
Jordan Cassady <jordan.cassady@gmail.com>
2024-10-21 00:16:59 +0000

    Make code better

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import sys  # Arbitrarily importing sys
-
 class Y:  # Short, cryptic class name
     def __call__(self, *args):  # Call method, minimalistic
         print(f"{' '.join(args[::-1])}")  # Reversing and printing args
